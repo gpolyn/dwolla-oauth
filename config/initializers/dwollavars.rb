eeed4281b6e594c5f6e5c92397bf835ca6732f8f
Gallagher Polyn <gallagher.polyn@gmail.com>
2016-03-18 10:12:59 -0400

    use environment variables

--- config/initializers/dwollavars.rb
+++ config/initializers/dwollavars.rb
@@ -3,10 +3,10 @@ require 'dwolla'
 module DwollaVars
 	mattr_reader  :Dwolla, :redirect
 	
-	@@redirect ||= "https://boiling-inlet-73544.herokuapp.com/dashboard/handle_oauth"
+	@@redirect ||= ENV['REDIRECT_URL'] # "https://boiling-inlet-73544.herokuapp.com/dashboard/handle_oauth"
 
 	@@Dwolla ||= Dwolla
-	@@Dwolla::api_key ||= "4eQRM9Bd4jJUzz3w0ML+mEXqCtlUrpcLqOvtI7P+74C2ukFC/l"
-	@@Dwolla::api_secret ||= "zHJT2CMZ8mahJzp7VUz+I37eUJmAs1oONAMhl7ldFzB5Lz/xRI"
+	@@Dwolla::api_key ||= ENV['API_KEY'] # "4eQRM9Bd4jJUzz3w0ML+mEXqCtlUrpcLqOvtI7P+74C2ukFC/l"
+	@@Dwolla::api_secret ||= ENV['API_SECRET'] # "zHJT2CMZ8mahJzp7VUz+I37eUJmAs1oONAMhl7ldFzB5Lz/xRI"
 	@@Dwolla::sandbox ||= true
 end
